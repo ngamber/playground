84ba413fcb121f024e106548012e2a60e72394ea
Nathan Gamber <ngamber@gmail.com>
2016-12-06 17:28:10 -0500

    added output for testing app

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update && sudo apt-get install ansible -y
 
 ansible-playbook playground.yml
+
+export IP=`curl -4 icanhazip.com`
+
+echo "Please visit the following url: http://$IP/"
